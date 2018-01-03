a8e33171cab2126995c43dc44b1889765585b13e
Yoanndp <yoann.diable@gmail.com>
2018-01-03 23:01:43 +0100

    Create settings.php
    This is the settings file that works with the core

---
+++ anticheat/settings.php
@@ -0,0 +1,11 @@
+<?php
+$starMargin = 1000;
+$localStars = 187; //This is the actual amount of stars for 2.1
+$_s = array(
+    "anticheat" => array(
+        "stars_limit" => $starMargin,
+        "local_stars" => $localStars
+    )
+);
+
+?>
