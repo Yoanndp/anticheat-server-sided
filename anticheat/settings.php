0d35f4d8b87de8270d13c23a71ac20ccd5146f0e
Yoanndp <yoann.diable@gmail.com>
2018-01-04 12:38:39 +0100

    Update settings.php
    Better like this

--- anticheat/settings.php
+++ anticheat/settings.php
@@ -1,10 +1,29 @@
 <?php
-$starMargin = 1000;
-$localStars = 187; //This is the actual amount of stars for 2.1
+$margin = 1000;
+$local_stars = 187; // From Stereo Madness to Fingerdash
+$local_coins = 64; //64 = (21 levels * 3 coins) + 1 (from the menu)
+$local_demons = 3; // Clubstep, ToE II and Deadlocked
+
 $_s = array(
     "anticheat" => array(
-        "stars_limit" => $starMargin,
-        "local_stars" => $localStars
+        "stars" => array(
+            "margin" => $margin,
+            "local" => $local_stars
+        ),
+
+        "coins" => array(
+            "margin" => $margin,
+            "local" => $local_coins 
+        ),
+
+        "usercoins" => array(
+            "margin" => $margin
+        ),
+
+        "demons" => array(
+            "margin" => $margin,
+            "local" => $local_demons
+        )
     )
 );
 
