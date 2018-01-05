2f95fba1d2014454443b0e541696725545bf4cc2
Yoanndp <yoann.diable@gmail.com>
2018-01-05 12:01:19 +0100

    Table anticheatLog
    Its to store when someone get banned

---
+++ anticheatLogs.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- Hôte :                        127.0.0.1
+-- Version du serveur:           5.7.11 - MySQL Community Server (GPL)
+-- SE du serveur:                Win32
+-- HeidiSQL Version:             9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Export de la structure de la table gdps21. anticheatlogs
+CREATE TABLE IF NOT EXISTS `anticheatlogs` (
+  `pk_accountID` int(11) NOT NULL,
+  `stars` int(11) NOT NULL,
+  `demons` int(11) NOT NULL,
+  `coins` int(11) NOT NULL,
+  `usercoins` int(11) NOT NULL,
+  `IP` varchar(50) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`pk_accountID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Les données exportées n'étaient pas sélectionnées.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
