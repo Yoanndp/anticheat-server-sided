d19564966d770a93b1a45911eae944acc7a726ea
Yoanndp <yoann.diable@gmail.com>
2018-01-10 12:19:31 +0100

    Update settings.php

--- anticheat/settings.php
+++ anticheat/settings.php
@@ -1,7 +1,7 @@
 <?php
 $margin = 1000;
-$local_stars = 187; // From Stereo Madness to Fingerdash
-$local_coins = 66; //64 = (21 levels * 3 coins) + 1 (from the menu) + 2 (glubfub and sparky from the vault)
+$local_stars = 190; // From Stereo Madness to Fingerdash + The challenge (3 stars)
+$local_coins = 66; //66 = (21 levels * 3 coins) + 1 (from the menu) + 2 (glubfub and sparky from the vault)
 $local_demons = 3; // Clubstep, ToE II and Deadlocked
 
 $_s = array(
